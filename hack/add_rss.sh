f7cb4c6fb54cd62d7b4ea0ec93dbc15891b644b9
mingfu <mingfu@alauda.io>
2024-09-27 08:42:08 +0800

    Add new article: Cursor 一个真正让程序员产生危机感的 AI 编程工具

---
+++ hack/add_rss.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+# 获取脚本的绝对路径
+SCRIPT_PATH=$(cd $(dirname $0); pwd)
+FEED_FILE=$SCRIPT_PATH/../feed/feed.xml
+
+python $SCRIPT_PATH/update_rss.py "$FEED_FILE" "$1" "$2"
+
+yq -p=xml -o=xml '
+.rss.channel.follow_challenge = {
+  "feedId": "62080294672416768",
+  "userId": "59875101284335616"
+}
+' $FEED_FILE > temp.xml && mv temp.xml $FEED_FILE
+
+git add feed.xml
+git commit -m "Add new article: $2"
+git push
---
+++ hack/update_rss.py
@@ -0,0 +1,53 @@
+import sys
+import feedparser
+import feedgenerator
+import datetime
+import time
+
+def parse_date(date_str):
+    if isinstance(date_str, time.struct_time):
+        return datetime.datetime(*date_str[:6])
+    return date_str
+
+def update_rss(feed_file,url, title):
+    # 读取现有的 RSS 文件
+    feed = feedparser.parse(feed_file)
+    print(feed)
+    
+    # 创建新的 RSS feed
+    new_feed = feedgenerator.Rss201rev2Feed(
+        title=feed.feed.title,
+        link=feed.feed.link,
+        description=feed.feed.description,
+    )
+    
+    # 添加新文章
+    new_feed.add_item(
+        title=title,
+        link=url,
+        pubdate=datetime.datetime.now(),
+        description=title
+    )
+    
+    # 添加现有文章
+    for entry in feed.entries:
+        new_feed.add_item(
+            title=entry.title,
+            link=entry.link,
+            pubdate=parse_date(entry.published_parsed),
+            description=entry.description
+        )
+    
+    # 写入更新后的 RSS 文件
+    with open(feed_file, 'w') as f:
+        new_feed.write(f, 'utf-8')
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python update_rss.py <url> <title>")
+        sys.exit(1)
+    
+    feed_file = sys.argv[1]
+    url = sys.argv[2]
+    title = sys.argv[3]
+    update_rss(feed_file, url, title)
