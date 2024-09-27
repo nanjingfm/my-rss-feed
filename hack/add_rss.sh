952b0cf42a2ad75c3222b854ee6289add1ab6a56
mingfu <mingfu@alauda.io>
2024-09-27 09:17:53 +0800

    Add new article: Usage: gum input [flags]
    Prompt for some input
    
    Flags:
      -h, --help                   Show context-sensitive help.
      -v, --version                Print the version number
    
          --placeholder="Type something..."
                                   Placeholder value ($GUM_INPUT_PLACEHOLDER)
          --prompt="> "            Prompt to display ($GUM_INPUT_PROMPT)
          --cursor.mode="blink"    Cursor mode ($GUM_INPUT_CURSOR_MODE)
          --value=""               Initial value (can also be passed via stdin)
          --char-limit=400         Maximum value length (0 for no limit)
          --width=0                Input width (0 for terminal width)
                                   ($GUM_INPUT_WIDTH)
          --password               Mask input characters
          --[no-]show-help         Show help keybinds ($GUM_INPUT_SHOW_HELP)
          --header=""              Header value ($GUM_INPUT_HEADER)
          --timeout=0              Timeout until input aborts ($GUM_INPUT_TIMEOUT)
    
    Style Flags
      --prompt.foreground=""         Foreground Color ($GUM_INPUT_PROMPT_FOREGROUND)
      --prompt.background=""         Background Color ($GUM_INPUT_PROMPT_BACKGROUND)
      --placeholder.foreground="240"
                                     Foreground Color
                                     ($GUM_INPUT_PLACEHOLDER_FOREGROUND)
      --placeholder.background=""    Background Color
                                     ($GUM_INPUT_PLACEHOLDER_BACKGROUND)
      --cursor.foreground="212"      Foreground Color ($GUM_INPUT_CURSOR_FOREGROUND)
      --cursor.background=""         Background Color ($GUM_INPUT_CURSOR_BACKGROUND)
      --header.foreground="240"      Foreground Color ($GUM_INPUT_HEADER_FOREGROUND)
      --header.background=""         Background Color ($GUM_INPUT_HEADER_BACKGROUND)

--- hack/add_rss.sh
+++ hack/add_rss.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+link=$1
+title=$2
+
+if [ -z "$link" ] || [ -z "$title" ]; then
+  link=$(gum input --prompt "Enter the link: ")
+  suggest_title=$(curl -sL $link | xmllint --html --xpath "//title/text()" - 2>/dev/null)
+  title=$(gum input --prompt "Enter the title: " --default="$suggest_title")
+fi
+
 # 获取脚本的绝对路径
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
-FEED_FILE=$SCRIPT_PATH/../feed/feed.xml
+FEED_FILE=$SCRIPT_PATH/../docs/feed.xml
 
-python $SCRIPT_PATH/update_rss.py "$FEED_FILE" "$1" "$2"
+python $SCRIPT_PATH/update_rss.py "$FEED_FILE" "$link" "$title"
 
 yq -p=xml -o=xml '
 .rss.channel.follow_challenge = {
@@ -13,6 +22,7 @@ yq -p=xml -o=xml '
 }
 ' $FEED_FILE > temp.xml && mv temp.xml $FEED_FILE
 
+cd $SCRIPT_PATH/../
 git add $FEED_FILE
-git commit -m "Add new article: $2"
+git commit -m "Add new article: $title"
 git push
